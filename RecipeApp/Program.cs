a1751bfe44b0d89ed36a00a5f01aaa67e242a50e
Victor M <Victor M@VICTOR-MUKOZI>
2023-04-26 10:48:52 +0200

    Add project files.

---
+++ RecipeApp/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace RecipeApplication
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+
+        public Ingredient(string name, double quantity, string unit)
+        {
+            Name = name;
+            Quantity = quantity;
+            Unit = unit;
+        }
+    }
+
+    class Step
+    {
+        public string Description { get; set; }
+
+        public Step(string description)
+        {
+            Description = description;
+        }
+    }
+
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("1. Display recipe");
+                Console.WriteLine("2. Scale recipe");
+                Console.WriteLine("3. Reset quantities");
+                Console.WriteLine("4. Clear recipe");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        recipe.Display();
+                        break;
+                    case 2:
+                        Console.Write("Enter scaling factor: ");
+                        double factor = double.Parse(Console.ReadLine());
+                        recipe.Scale(factor);
+                        break;
+                    case 3:
+                        recipe.Reset();
+                        break;
+                    case 4:
+                        recipe.Clear();
+                        recipe = new Recipe();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ RecipeApp/Recipe.cs
@@ -0,0 +1,81 @@
+﻿using RecipeApplication;
+
+class Recipe
+{
+    private Ingredient[] ingredients;
+    private Step[] steps;
+
+    public Recipe()
+    {
+        Console.Write("Enter the number of ingredients: ");
+        int numIngredients = int.Parse(Console.ReadLine());
+
+        ingredients = new Ingredient[numIngredients];
+
+        for (int v = 0; v < numIngredients; v++)
+        {
+            Console.Write($"Enter the name of ingredient {v + 1}: ");
+            string name = Console.ReadLine();
+
+            Console.Write($"Enter the quantity of {name}: ");
+            double quantity = double.Parse(Console.ReadLine());
+
+            Console.Write($"Enter the unit of measurement for {name}: ");
+            string unit = Console.ReadLine();
+
+            ingredients[v] = new Ingredient(name, quantity, unit);
+        }
+
+        Console.Write("Enter the number of steps: ");
+        int numSteps = int.Parse(Console.ReadLine());
+
+        steps = new Step[numSteps];
+
+        for (int v = 0; v < numSteps; v++)
+        {
+            Console.Write($"Enter the description for step {v + 1}: ");
+            string description = Console.ReadLine();
+
+            steps[v] = new Step(description);
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Ingredients:");
+
+        foreach (Ingredient ingredient in ingredients)
+        {
+            Console.WriteLine($"{ingredient.Quantity} {ingredient.Unit} {ingredient.Name}");
+        }
+
+        Console.WriteLine("Steps:");
+
+        for (int v = 0; v < steps.Length; v++)
+        {
+            Console.WriteLine($"{v + 1}. {steps[v].Description}");
+        }
+    }
+
+    public void Scale(double factor)
+    {
+        foreach (Ingredient ingredient in ingredients)
+        {
+            ingredient.Quantity *= factor;
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (Ingredient ingredient in ingredients)
+        {
+            ingredient.Quantity /= 2;
+        }
+    }
+
+    public void Clear()
+    {
+        Array.Clear(ingredients, 0, ingredients.Length);
+        Array.Clear(steps, 0, steps.Length);
+    }
+}
